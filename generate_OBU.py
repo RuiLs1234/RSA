6d3be8975b6ff3f872948b8afbf518aa00320010
RuiLs1234 <119522445+RuiLs1234@users.noreply.github.com>
2024-05-30 20:22:37 +0100

    Add files via upload

--- generate_OBU.py
+++ generate_OBU.py
@@ -8,7 +8,16 @@ global_count = []
 obj = {}
 latitude = []
 longitude = []
+total_RSU_latitude = []
+total_RSU_longitude = []
+point_reach = 0
 
+def get_ips_from_docker_compose(filename):
+    with open(filename, 'r') as file:
+        docker_config = yaml.safe_load(file)
+        rsu_ips = [docker_config['services'][service]['networks']['vanetzalan0']['ipv4_address'] for service in docker_config['services'] if service.startswith('rsu')]
+        central_broker_ip = docker_config['services']['central']['networks']['vanetzalan0']['ipv4_address']
+        return rsu_ips, central_broker_ip
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -16,68 +25,63 @@ def on_connect(client, userdata, flags, rc):
     # client.subscribe("vanetza/out/denm")
     # ...
 
-
-# É chamada automaticamente sempre que recebe uma mensagem nos tópicos subscritos em cima
 def on_message(client, userdata, msg):
-    message = msg.payload.decode('utf-8')
     global obj
     
     print('Topic: ' + msg.topic)
-    print('Message' + message)
+    print('Message' + msg.payload.decode('utf-8'))
 
-    objt = json.loads(message)
+    objt = json.loads(msg.payload.decode('utf-8'))
     obj = objt
 
-    # lat = obj["latitude"]
-    # ...
-    
 def generate(i):
     global longitude
     global latitude
-    if i == 0:
-        f = open('examples/in_cam.json')
-        m = json.load(f)
-        m["accEngaged"] = "Hello"
-        m["latitude"] = latitude[i]
-        latitude[i] = latitude[i]+1
-        m["longitude"] = longitude[i]
-        longitude[i] = longitude[i]+1
-        m = json.dumps(m)
-        client.publish("vanetza/in/cam",m)
-        f.close()
-        sleep(1) 
-    else:
-        f = open('examples/in_cam.json')
-        m = json.load(f)
-        m["accEngaged"] = "Hello part"
-        m["latitude"] = latitude[i]
-        latitude[i] = latitude[i]+1
-        m["longitude"] = longitude[i]
-        longitude[i] = longitude[i]+1
-        m = json.dumps(m)
-        client.publish("vanetza/in/cam",m)
-        f.close()
-        sleep(1) 
+    global point_reach
+    global total_RSU_latitude
+    global total_RSU_longitude
 
+    if i == 0:
+        with open('examples/in_cam.json') as f:
+            m = json.load(f)
+            m["heading"] = i
+            m = json.dumps(m)
+            client.publish("vanetza/in/cam", m)
+            sleep(1)
+        f.close()
+    else:
+        with open('examples/in_cam.json') as f:
+            m = json.load(f)
+            m["heading"] = i
+            m = json.dumps(m)
+            client.publish("vanetza/in/cam", m)
+            sleep(1)
+        f.close()
+
+
+
+total_RSU = int(sys.argv[1])
+total_ovu = int(sys.argv[2])
+
+rsu_ips, central_broker_ip = get_ips_from_docker_compose('docker-compose.yml')
+
+global central_broker
 client = mqtt.Client()
+client.connect(central_broker_ip, 1883)
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("192.168.98.10", 1883, 60)
-total_ovu = sys.argv[2]
-total_OVU_latitude = sys.argv[5]
-total_OVU_longitude = sys.argv[6]
-if latitude == []:
-    i = 0
-    while total_ovu > i:
-        latitude.append(total_OVU_latitude[5][i])
-        longitude.append(total_OVU_longitude[6][i])
-        i = i+1
 
+
+total_RSU_latitude = list(map(float, sys.argv[3].split(',')))
+total_RSU_longitude = list(map(float, sys.argv[4].split(',')))
+do_not_do = sys.argv[6]
+
+for i in range(total_ovu):
+    latitude.append(0)
+    longitude.append(0)
 
 threading.Thread(target=client.loop_forever).start()
 
-while(True):
-    i = 0
-    while i < total_ovu:
-        generate(i)
-        i = i+1
+while True:
+    for i in range(total_ovu):
+        generate(i, total_RSU)
